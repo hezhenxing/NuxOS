0d42557196d71303664ef31d995c6b3848e4802e
He Zhenxing <hezhenxing@gmail.com>
2025-09-22 21:36:45 +0800

    zsh: command not found handler support choosing packages to install

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -23,9 +23,24 @@
     ];
     initContent = ''
       function command_not_found_handler() {
-        cmd=`command-not-found "$1" |& sed -n '3p'`
-        if [ -n "$cmd" ]; then
-          eval "$cmd --command \"$@:q\""
+        pkgs=`command-not-found "$1" |& sed -n 's/^  nix-shell -p \(.*\)/\1/p'`
+        if [ -n "$pkgs" ]; then
+          echo "The program '$1' is not in your PATH. You can make it available by temporarily install one of the following packages:" 1>&2
+          PS3="Choose a package or press Ctrl-C to cancel: "
+          setopt shwordsplit
+          select pkg in $pkgs; do
+            case $pkg in
+              "")
+                echo "Invalid option, please try again!" 1>&2
+                ;;
+              *)
+                break
+                ;;
+            esac
+          done
+          unsetopt shwordsplit
+          echo "You chose package '$pkg'" 1>&2
+          nix-shell -p $pkg --command "$@:q"
         else
           echo "command not found: $1" 1>&2
           return 127
